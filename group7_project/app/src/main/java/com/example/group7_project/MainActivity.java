a023c27512b2602dd3bece71a5c1a5dfc2f5da02
Karl Öqvist <karloq@student.chalmers.se>
2020-11-09 15:29:36 +0100

    Stor XML-uppdatering. Menyn är implementerad

--- group7_project/app/src/main/java/com/example/group7_project/MainActivity.java
+++ group7_project/app/src/main/java/com/example/group7_project/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.group7_project;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+    boolean isDrawerOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+
+        ImageButton open_drawer = findViewById(R.id.button_search_menu);
+        open_drawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawer.openDrawer(GravityCompat.START);
+                isDrawerOpen = true;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(isDrawerOpen) {
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
