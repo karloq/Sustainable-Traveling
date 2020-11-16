4df4c7e554aa23acfd80ace2e404daabb32422d8
Karl Öqvist <karloq@student.chalmers.se>
2020-11-16 21:06:04 +0100

    Filtermeny klar

--- group7_project/app/src/main/java/com/example/group7_project/FilterActivity.java
+++ group7_project/app/src/main/java/com/example/group7_project/FilterActivity.java
@@ -1,17 +1,34 @@
 package com.example.group7_project;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FilterActivity extends AppCompatActivity {
+    ImageButton button_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
 
+        button_back = findViewById(R.id.button_filter_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                applyFilter();
+            }
+        });
 
+    }
+
+    private void applyFilter() {
+        Intent data = new Intent();
+        setResult(RESULT_OK, data);
+        finish();
     }
 }
