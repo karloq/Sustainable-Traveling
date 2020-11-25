fdc15ef19d9be493fa028c2bd58f95aa48ceae20
HelmrothF <helmroth@student.chalmers.se>
2020-11-25 17:21:55 +0100

    Logic for the sustainability page

---
+++ group7_project/app/src/main/java/com/example/group7_project/GlobalSustainabilityData.java
@@ -0,0 +1,37 @@
+package com.example.group7_project;
+
+import android.app.Application;
+
+/*
+Global class to store relevant data for the sustainability page
+Actions from MainActivity/other will influence the variables
+Hardcoded numbers before everything is implemented
+
+To implement:
+GlobalSustainabilityData variableName = (GlobalSustainabilityData) getApplicationContext(); in java class
+variableName.set/get to get/set the global variables
+
+*/
+public class GlobalSustainabilityData extends Application {
+
+    // Number of leafs collected during the month
+    private int leafCounter = 37;
+    // Number of total collected green trees
+    private int greenTreeCounter = 3;
+    // Number of total collected golden trees
+    private int goldTreeCounter = 1;
+    // Rank of the users performance versus other users, 1 to 5
+    private int rank = 5;
+
+
+    /* SETTERS AND GETTERS */
+    public int getLeafCounter() {return leafCounter;}
+    public void setLeafCounter(int leafCounter) {this.leafCounter = leafCounter;}
+    public int getGreenTreeCounter() {return greenTreeCounter;}
+    public void setGreenTreeCounter(int greenTreeCounter) {this.greenTreeCounter = greenTreeCounter;}
+    public int getGoldTreeCounter() {return goldTreeCounter;}
+    public void setGoldTreeCounter(int goldTreeCounter) {this.goldTreeCounter = goldTreeCounter;}
+    public int getRank() {return rank;}
+    public void setRank(int rank) {this.rank = rank;}
+
+}
--- group7_project/app/src/main/java/com/example/group7_project/SustainabilityPageActivity.java
+++ group7_project/app/src/main/java/com/example/group7_project/SustainabilityPageActivity.java
@@ -1,17 +1,81 @@
 package com.example.group7_project;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SustainabilityPageActivity extends AppCompatActivity {
-    /* TODO: Make class
-    *   TODO: Make xml
-    *    TODO: Implement graphics */
+    ImageView picTree, picRanking;
+    TextView rankText, greenTrees, goldTrees, leafs, leafsRemaining;
+    int toNextLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sustainability_page);
+
+        // Connects the variables in the XML-file to this  class
+        picTree = (ImageView) findViewById(R.id.treeLevel);
+        picRanking = (ImageView) findViewById(R.id.picTravelRanking);
+        rankText = (TextView) findViewById(R.id.text_scoreboard);
+        greenTrees = (TextView) findViewById(R.id.nbGreenTrees);
+        goldTrees = (TextView) findViewById(R.id.nbGoldenTrees);
+        leafs = (TextView) findViewById(R.id.nbLeafs);
+        leafsRemaining = (TextView) findViewById(R.id.levelDescription);
+
+        // To be able to manage our global variables
+        GlobalSustainabilityData globalSustainabilityData = (GlobalSustainabilityData) getApplicationContext();
+
+        // Set rank level, tree level, leaf counter, green tree counter, gold tree counter & leafs remaining
+        setRankLevel(globalSustainabilityData.getRank());
+        setTreeLevel(globalSustainabilityData.getLeafCounter());
+        greenTrees.setText(String.valueOf(globalSustainabilityData.getGreenTreeCounter()));
+        goldTrees.setText(String.valueOf(globalSustainabilityData.getGoldTreeCounter()));
+        leafs.setText(String.valueOf(globalSustainabilityData.getLeafCounter()));
+        leafsRemaining.setText("För att nå nästa nivå behöver du samla " + String.valueOf(toNextLevel) + " miljöresor");
+
+    }
+
+    // Decides which tree level to show on the sustainability page
+    // Needs to manually update the levels in case of new playing rules
+    public void setTreeLevel(int nbLeafs){
+        if(nbLeafs < 3){
+            picTree.setImageResource(R.drawable.tree_1);
+            toNextLevel = 3 - nbLeafs;
+        } else if(nbLeafs < 6){
+            picTree.setImageResource(R.drawable.tree_2);
+            toNextLevel = 6 - nbLeafs;
+        } else if(nbLeafs < 10){
+            picTree.setImageResource(R.drawable.tree_3);
+            toNextLevel = 10 - nbLeafs;
+        } else if(nbLeafs < 15){
+            picTree.setImageResource(R.drawable.tree_4);
+            toNextLevel = 15 - nbLeafs;
+        } else if(nbLeafs < 21){
+            picTree.setImageResource(R.drawable.tree_5);
+            toNextLevel = 21 - nbLeafs;
+        } else if(nbLeafs < 31){
+            picTree.setImageResource(R.drawable.tree_6);
+            toNextLevel = 31 - nbLeafs;
+        } else {
+            picTree.setImageResource(R.drawable.tree_7);
+            toNextLevel = 0;
+        }
+    }
+
+    // Calculates the users rank in comparison to other users
+    public void setRankLevel(int rankLevel){
+
+        //TODO: Bestämma en hur vi ska räkna ut detta. Behövs det?
+
+        switch(rankLevel){
+            case 1: picRanking.setImageResource(R.drawable.toptravelers20); rankText.setText(R.string.sustainability_relative_20); break;
+            case 2: picRanking.setImageResource(R.drawable.toptravelers40); rankText.setText(R.string.sustainability_relative_40); break;
+            case 3: picRanking.setImageResource(R.drawable.toptravelers60); rankText.setText(R.string.sustainability_relative_60); break;
+            case 4: picRanking.setImageResource(R.drawable.toptravelers80); rankText.setText(R.string.sustainability_relative_80); break;
+            case 5: picRanking.setImageResource(R.drawable.toptravelers99); rankText.setText(R.string.sustainability_relative_99); break;
+        }
     }
 }
