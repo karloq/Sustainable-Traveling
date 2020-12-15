4d7ce1abbb0c45d12e71ce6b475201e6113fdabc
Karl Öqvist <karloq@student.chalmers.se>
2020-12-15 11:58:35 +0100

    Liten ändring

--- group7_project/app/src/main/java/com/example/group7_project/SustainabilityPageActivity.java
+++ group7_project/app/src/main/java/com/example/group7_project/SustainabilityPageActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -158,7 +157,7 @@ public class SustainabilityPageActivity extends AppCompatActivity {
         goldTrees.setText(String.valueOf(getGoldTrees()));
         leafs.setText(String.valueOf(userData.getLeafCounter()));
 
-        String string = "För att nå nästa nivå behöver du samla " + toNextLevel + " miljöresor ";
+        String string = "For the next level you need to gather " + toNextLevel + " sustainable trips ";
         leafsRemaining.setText(string);
 
         setRankLevel(userData.getRank());
